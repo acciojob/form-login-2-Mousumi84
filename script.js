c2baa7aed0b78b9e3259344829bc8a0cfcecd510
Mousumi Dsa <58068282+Mousumi84@users.noreply.github.com>
2024-02-22 03:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function alertbox() {
-	let fn=document.getElementBy("fn");
-	alert("First Name:"+fn);
+	let fn=document.getElementById("fn");
+	let ln=document.getElementById("ln");
+	let pn=document.getElementById("pn");
+	let ei=document.getElementById("ei");
+	alert("First Name: "+fn.value+" Last Name: "+ln.value+" Phone Number: "+pn.value+" Email ID: "+ei.value);
 }
+
 
